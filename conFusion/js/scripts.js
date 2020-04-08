8e4c1fac70c996f1406d3682feeebaf4ce7ca7ec
Diwash Pradhan <diwashp@gmail.com>
2020-04-08 15:05:51 +0545

    NPM Part 1

---
+++ conFusion/js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+
+  $('#mycarousel').carousel({interval:1000 });
+
+  $('#carousel-button').click(function(){
+    if($('#carousel-button').children('span').hasClass("fa-pause")){
+      $('#mycarousel').carousel('pause');
+      $('#carousel-button').children('span').removeClass("fa-pause")
+      $('#carousel-button').children('span').addClass("fa-play")
+    }
+    else if($('#carousel-button').children('span').hasClass("fa-play")){
+      $('#mycarousel').carousel('cycle');
+      $('#carousel-button').children('span').removeClass("fa-play");
+      $('#carousel-button').children('span').addClass("fa-pause");
+    }
+  });
+
+
+
+
+
+  $("#reserveBtn").click(function(){
+      $("#reserveModal").show();
+  });
+  $("#loginBtn").click(function(){
+      $("#loginModal").show();
+  });
+
+
+  $("#close").click(function(){
+    $('#reserveModal').hide();
+  });
+  $("#closeLogin").click(function(){
+    $('#loginModal').toggle();
+  });
+
+
+});
